8e44d16f6dcd150354cea319d558b31f25d4f329
Vatan Soni <vatansoni89@gmail.com>
2019-08-19 20:23:13 +0530

    class to fun compo using useState and useEffect Hooks

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,42 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  });
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map(c => {
-              return (
-                <tr key={c.id}>
-                  <td>{c.title}</td>
-                  <td>{c.authorId}</td>
-                  <td>{c.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(c => {
+            return (
+              <tr key={c.id}>
+                <td>{c.title}</td>
+                <td>{c.authorId}</td>
+                <td>{c.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;
