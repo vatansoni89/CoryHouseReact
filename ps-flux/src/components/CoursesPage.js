bf36ffb1a20c40e5f694fd0270afc8aebf8b9251
Vatan Soni <vatansoni89@gmail.com>
2019-08-28 16:36:09 +0530

    More comments on Hooks

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
 function CoursesPage() {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState([]); //useState Hook
 
   //Calling setCourses causes the compo to re-render, which caused useEffect to re-run.
   //The dependency array is a list of values that useEffect should watch. It re-runs when values in this array change.
   useEffect(() => {
+    //useEffect Hook
     getCourses().then(_courses => setCourses(_courses));
-  }, []);
+  }, []); // second arg [] is dependency array, which is observed after each fun call. it array don't have chage then no further re-render.
 
   return (
     <>
