5cb1f1e9f85d25a53a6546b91693fef9c8704b5f
Vatan Soni <vatansoni89@gmail.com>
2019-09-02 23:45:36 +0530

    Prompt on Transition

--- ps-flux/src/components/ManageCoursePage.js
+++ ps-flux/src/components/ManageCoursePage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursepage = props => {
   //debugger;
   return (
     <>
       <h2>Manage Course</h2>
+      <Prompt when={true} message="Are you sure to leave???" />
       {props.match.params.slug}
     </>
   );
